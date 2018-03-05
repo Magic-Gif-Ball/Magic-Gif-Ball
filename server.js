93835e01842390d60f65c35ee7910038bdcb90f1
Beverly Pham <beverly.pham@gmail.com>
2018-03-05 13:20:05 -0800

    deploy public

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const pg = require('pg');
 const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.POST;
-const CLIENT_URL = process.env.CLIENT_URL;
+// const CLIENT_URL = process.env.CLIENT_URL;
 const TOKEN = process.env.TOKEN;
 
 const client = new pg.Client(process.env.DATABASE_URL);
@@ -16,10 +16,12 @@ client.on('error', err => console.error(err));
 
 app.use(express.static('./public'));
 
-app.use(cors());
+// app.use(cors());
 
-app.get('/', (req, res) => res.send('This is a test'));
+// app.get('/', (req, res) => res.send('This is a test'));
 
-app.get('*', (req, res) => res.redirect(CLIENT_URL));
+app.get('/', (req, res) => res.sendFile('index.html', {root:'./public'})
+
+app.get('*', (req, res) => res.sendFile('index.html', {root:'./public'})
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
