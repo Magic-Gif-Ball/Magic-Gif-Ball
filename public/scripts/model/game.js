1b33e3b5a46756bc76cc55367e62c264180eb4a3
Patricia G Raftery <praftery8@gmail.com>
2018-03-05 16:10:08 -0800

    added error view

--- public/scripts/model/game.js
+++ public/scripts/model/game.js
@@ -1,2 +1,16 @@
 'use strict';
 
+var app = app || {};
+var __API_URL__ = 'http://localhost:3000';
+
+(function(module) {
+
+  //do we want an error view?
+  function errorCallback(err) {
+    console.error(err);
+    module.errorView.initErrorPage(err);
+  }
+
+//module
+})(app)
+
