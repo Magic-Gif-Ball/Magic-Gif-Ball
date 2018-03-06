bc70c93e8c81346081a9a627d5720e3d063dee47
Beverly Pham <beverly.pham@gmail.com>
2018-03-06 11:43:28 -0800

    add tag to ajax request

--- public/scripts/model/game.js
+++ public/scripts/model/game.js
@@ -12,11 +12,24 @@ var __API_URL__ = 'http://localhost:3000';
   }
 
   Game.fetchGif = function() {
-    $.get(`${__API_URL__}/api/v1/gif/random`)
+    let index = Game.randomArrayIndex();
+    let tag = randomArray[index];
+    console.log(tag);
+    // $.get(`${__API_URL__}/api/v1/gif/random`)
+    $.ajax({
+      url: `${__API_URL__}/api/v1/gif/random`,
+      data: {tag}
+    })
       .then(result => {
         console.log(result);
         $('#question-form img').attr('src', result);
       });
+  };
+
+  let randomArray = ['yes', 'no', 'maybe', 'try again', 'outlook unclear', 'hell no', 'hell yes'];
+
+  Game.randomArrayIndex = () => {
+    return Math.floor(Math.random() * (randomArray.length - 1 - 0 + 1)) + 0;
   };
 
 
