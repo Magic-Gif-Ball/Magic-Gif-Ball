211471688e2c4c19d734cfc1a8feea3d516859c6
Beverly Pham <beverly.pham@gmail.com>
2018-03-07 13:13:41 -0800

    update view populating with tags

--- public/scripts/view/updateView.js
+++ public/scripts/view/updateView.js
@@ -8,10 +8,15 @@ var app = app || {};
   updateView.initUpdatePage = () => {
     $('.container').hide();
     $('.update-View').show();
+    let userInfo = {
+      user_id: localStorage.userId,
+      tagArray: localStorage.tagArray.split(','),
+    };
     let template = Handlebars.compile($('#update-template').text());
-    $('.update-View').append(template);
-
+    $('.update-View').append(template(userInfo));
   };
+
+  
 
   module.updateView = updateView;
 })(app);
