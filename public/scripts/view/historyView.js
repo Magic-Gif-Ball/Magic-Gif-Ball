1e87c05b3a70a4170dfe58a95020a69fa6a4c808
Patricia G Raftery <praftery8@gmail.com>
2018-03-06 16:59:11 -0800

    empty history container before appending

--- public/scripts/view/historyView.js
+++ public/scripts/view/historyView.js
@@ -9,6 +9,7 @@ var app = app || {};
   //when click history button, trigger history view and populate history
   historyView.initHistoryPage = function(ctx, next) {
     $('.container').hide();
+    $('.history').empty();
     $('.history-View').show();
     module.Game.all.map(game => $('.history').append(game.toHtml()));
     next()
